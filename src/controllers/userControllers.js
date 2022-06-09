e8c2149092f02427db5fb990d44b20a81f75bbd4
MaxiColombo~ <maximocolombo2003@gmail.com>
2022-06-09 20:48:02 -0300

    Express Validator Login Register

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require('fs');
 const bcrypt = require ("bcryptjs");
+const { validationResult } = require("express-validator");
 
 const usersFilePath = path.join(__dirname, '../data/users/usersDataBase.json');
 const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
@@ -12,8 +13,19 @@ const userControllers = {
         res.render("./users/register")
     },
     sendRegister : (req, res)=> {
+        //Express validator
+        let resultValidation = validationResult(req);
+        
+        if (resultValidation.errors.length > 0){
+            return res.render("./users/register",{errors: resultValidation.mapped(),/* old:req.body */});
+        }else{
+            res.redirect("/")
+        }
+        
+        //Encriptar contraseña
         let pass = bcrypt.hashSync(req.body.password, 10)
-        let newUser = {
+        //Formularios
+        let newUser = { 
             identificador: users[users.length - 1].identificador + 1,
             name: req.body.name,
             lastname:req.body.lastname,
@@ -26,18 +38,26 @@ const userControllers = {
         }
         users.push(newUser);
         fs.writeFileSync(usersFilePath, JSON.stringify(users,null," "));
-        res.redirect("/")
+        /* res.redirect("/users/register") */
+
+    },
+    sendToRegister:(req, res)=> {
+        res.redirect("/users/register")
     },
     login: (req, res)=> {
         res.render("./users/login")
     },
     
     sendLogin:(req, res)=> {
-        res.send("Se logueo correctamente")
+        let resultValidation = validationResult(req);
+        
+        if (resultValidation.errors.length > 0){
+            return res.render("./users/login",{errors: resultValidation.mapped(),/* old:req.body */});
+        }else{
+            res.redirect("/")
+        }
     },
-    sendToRegister:(req, res)=> {
-        res.redirect("/users/register")
-    },
+    
     sendLoginGoogle:(req, res)=> {
         res.send("Se logueo correctamente con Google")
     },
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const {body} = require("express-validator");
 
 const userControllers = require("../controllers/userControllers.js")
-
+//multer image
 const storage = multer.diskStorage({
     destination:function (req,file,callback){
     callback(null,"public/img/users");
@@ -13,27 +14,35 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({storage: storage});
+//express validator
+const validateRegister = [
+body("name").notEmpty().withMessage("Debe completar el nombre"),/* .bail().isLength({max:20}).withMessage("El maximo es de 20 caracteres"), */
+body("lastname").notEmpty().withMessage("Debe completar el apellido"),/* .bail().isLength({max:20}).withMessage("El maximo es de 20 caracteres"), */
+body("email")/* .isEmail() */.notEmpty().withMessage("Debe completar el email"),/* .bail().isLength({max:70}).withMessage("El maximo es de 70 caracteres"), */
+body("dni").notEmpty()/* .isNumeric() */.withMessage("Debe completar el dni"),/* .bail().isLength({max:8}).withMessage("El maximo es de 8 caracteres"), */
+body("adress").notEmpty().withMessage("Debe completar el domicilio"),/* .bail().isLength({max:70}).withMessage("El maximo es de 70 caracteres"), */
+body("password").notEmpty().withMessage("Debe completar la contraseña"),/* .bail().isLength({max:30}).withMessage("El maximo es de 30 caracteres"), */
+body("passwordConfirm").notEmpty().withMessage("Debe confirmar la contraseña")/* .bail().isLength({max:30}).withMessage("El maximo es de 30 caracteres"), */
+]
+const validateLogin=[
+body("email").notEmpty().withMessage("Debe completar el email"),
+body("password").notEmpty().withMessage("Debe completar completar la contraseña")
+]
 
 
-
-
+//mostramos las vistas
 router.get("/register", userControllers.register);
 router.get("/login",userControllers.login)
 //cartel se logueo
-router.post("/log",userControllers.sendLogin)
+router.post("/log",validateLogin,userControllers.sendLogin)
 //nos manda de login a register
 router.post("/reg",upload.single("file-image-user"), userControllers.sendToRegister)
 
 
 
 
-
-
-
-
-
 //cartel de se registro
-router.post("/registrado",userControllers.sendRegister)
+router.post("/registrado",validateRegister,userControllers.sendRegister)
 //cartel se logueo con google
 router.post("/reg/google",userControllers.sendLoginGoogle)
 //cartel se logueo con facebook
