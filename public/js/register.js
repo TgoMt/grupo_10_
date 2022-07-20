01ad7859e71e5341df6f153c7c1841cde1b9ce18
Valentin-Lau <val.laurenzano@gmail.com>
2022-07-19 21:35:35 -0300

    resigter.js mas cambios raris

--- public/js/register.js
+++ public/js/register.js
@@ -14,29 +14,62 @@ window.onload = function(){
               let email = document.querySelector("#email")
               let password = document.querySelector("#password")
               let errors=[]
-              if(email.value==""){
+           /*    if(email.value==""){
                   errors.push("Debe completar el email")
               }else{
                   formulario.password.focus()
-              }
+              } */
       
-              let regEmail = /\S+@\S+\.\S+/;
+            /*   let regEmail = /\S+@\S+\.\S+/;
               if(!regEmail.test(email.value)){
                   errors.push("Debe ser un formato de email valido")
               
-              }
+              } */
 
               if(name.value == ""){
-                    errors.push("Debe Ingresar un nombre")
-                }else if (name.length < 2){
-                    errors.push("El minimo es de 2 caracteres") 
+                    errors.push("Debe ingresar un nombre")
+                }else if (name.value.length < 2){
+                    errors.push("El minimo del" + " nombre " + "es de 2 caracteres") 
                 }else{
                     formulario.lastname.focus()
                }
 
+               
+              if(lastname.value == ""){
+                    errors.push("Debe ingresar un apellido")
+                }else if (lastname.value.length < 2){
+                    errors.push("El minimo del" + " apellido " + "es de 2 caracteres") 
+                }else{
+                    formulario.dni.focus()
+               }
 
-      
-      
+               /* let file = req.file;
+               let fileExt = path.extname(file.originalname);
+               let acceptedExt = [".jpg",".jpeg",".png",".gif"]
+               if(!file){
+                   errors.push("Debe subir una imagen")
+                  }else if (!acceptedExt.includes(fileExt)){
+                    errors.push("Las extenciones aceptadas son"+ ", jpg"+", png"+", jpeg" +", gif")
+                   } */
+
+                   if(dni.value == ""){
+                    errors.push("Debe ingresar un dni")
+                }else if (dni.value.length < 9){
+                    errors.push("El minimo del" + " dni " + "es de 9 caracteres") 
+                }else{
+                    formulario.role.focus()
+               }     
+
+
+
+
+
+
+               
+
+
+
+
       
       
               if (errors.length > 0) {
