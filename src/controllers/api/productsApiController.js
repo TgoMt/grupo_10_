97b592e2961882b8db7f74339781da117217e741
MaxiColombo~ <maximocolombo2003@gmail.com>
2022-08-03 02:04:43 -0300

    GruoBy no funcional, error en la logica

--- src/controllers/api/productsApiController.js
+++ src/controllers/api/productsApiController.js
@@ -1,52 +1,50 @@
 const path = require("path");
 const sequelize = require("sequelize");
 const Op = sequelize.Op;
-const groupBy = require("array.prototype.groupby")
-groupBy.shim();
+
 
 
 const db = require(path.join(__dirname, "../../../database/models"));
 
 module.exports = {
-    list: (req, res) => {
-        let products = db.Product.findAll()
-        let category = db.Category.findAll()
-        Promise.all([products,category]).then(function([product,categories]){
+
+    list: (req,res) => {
+        db.Product.findAll({attributes: ["categoryId"],group: "categoryId"})
+        .then(products => {
             return res.status(200).json({
-                
-                /*   */
-                total: products.length,
-                countCategory: 
-                categories.map(function (parametro, a, categoriesMap) {
-               return product.map(function (parametro, i, productMap){
-            if (productMap[i].categoryId === categoriesMap[a].id){
-             return productMap[i].name
-            } /* else if (productMap[i].categoryId  === 2){
-             licores.push(productMap[i])
-            }else if (productMap[i].categoryId  === 3){
-             bebidasBlancas.push(productMap[i])
-            }else if (productMap[i].categoryId === 4){
-             aperitivos.push(productMap[i])
-            } */
+            products
             })
-             }),
-             
-            /* var categoriesGroup = {
-                cervezas: cervezas,
-                licores: licores,
-                bebidasBlancas: bebidasBlancas,
-                aperitivos: aperitivos
-               } */
+        })
         
-             
-            status: 200
-            
-        });
-            })
-          
-            
-           
+        
     },
+
+
+
+//(FINDALL({attributes: ["categoryId",[sequelize.fn("COUNT", sequelize.col("categoryId")), "cerveza"],],group: "categoryId"}))
+
+   /*  list: (req, res) => {
+        let products = db.Product.findAll()//count group by  
+        let category = db.Category.findAll()
+        Promise.all([products, category]).then(function ([product, categories]) {
+            return res.status(200).json({
+
+                total: products.length,
+                countCategory:
+                    categories.map(function (parametro, a, categoriesMap) {
+                        return product.map(function (parametro, i, productMap) {
+                            if (productMap[i].categoryId === categoriesMap[a].id) {
+                                return productMap[i].name
+                            } else {
+                                return {}//no sabemos como hacer para que no aparezca nada
+                            }
+                        })
+                    }),
+                status: 200
+                
+            });
+        })
+    }, */
     showOne: (req, res) => {
         db.Product.findByPk(req.params.id)
             .then(product => {
@@ -60,36 +58,36 @@ module.exports = {
     createProduct: (req, res) => {
         db.Product.create(req.body)
         db.Category.findAll()
-        .then(product => {
-            return res.status(200).json({
-                data: product,
-                status:200,
-                created:"Se creo"
+            .then(product => {
+                return res.status(200).json({
+                    data: product,
+                    status: 200,
+                    created: "Se creo"
+                })
             })
-        })
     },
     deleteProduct: (req, res) => {
-        db.Product.destroy({where:{id:req.params.id}})
-        .then(response => {
-            return res.json(response)
-        })
+        db.Product.destroy({ where: { id: req.params.id } })
+            .then(response => {
+                return res.json(response)
+            })
     },
     searchProductByName: (req, res) => {
         db.Product.findAll({
-            where:{
-                name:{[Op.like]: "%"+req.query.keyword+"%"}
-        }
-    })
-    .then(products => {
-        if(products.length > 0){
-        return res.status(200).json(products)
-    }
-        return res.status(200).json("No se encontraron productos")
-    
-    })
+            where: {
+                name: { [Op.like]: "%" + req.query.keyword + "%" }
+            }
+        })
+            .then(products => {
+                if (products.length > 0) {
+                    return res.status(200).json(products)
+                }
+                return res.status(200).json("No se encontraron productos")
+
+            })
 
     },
-    
+
 
 
 
