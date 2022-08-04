ca16b7d0d6a163f7e9a68dfe16a405fc1252157b
MaxiColombo~ <maximocolombo2003@gmail.com>
2022-08-04 20:00:38 -0300

    APIS TERMINADAS product id

--- src/controllers/api/productsApiController.js
+++ src/controllers/api/productsApiController.js
@@ -1,7 +1,7 @@
 const { group } = require("console");
 const path = require("path");
 const Sequelize = require('sequelize');
-
+//Sequelize Raw Query (Para pasar el codigo directamente de SQL (sigue abajo))
 const sequelize = new Sequelize('sipintadb', 'root', '', {
     host: 'localhost',
     dialect: 'mysql',
@@ -17,9 +17,10 @@ module.exports = {
     list: (req,res) => {
         db.Product.findAll().then(producto => {
 
-        
+        //Sequelize Raw Query (aca le pasamos el codigo con toda la estructura)
         sequelize.query("select category,count(*) as Conteo from categories inner join products on categoryId=categories.id group by categoryId")
         .then(products => {
+            //esto va con sequelize raw query
         [countByCategory, metadata] = products
             return res.status(200).json({
                 total: producto.length,
@@ -71,13 +72,37 @@ module.exports = {
         })
     }, */
     showOne: (req, res) => {
-        db.Product.findByPk(req.params.id)
-            .then(product => {
-                return res.status(200).json({
-                    data: product,
-                    status: 200
-                });
-            });
+       let productOrder = db.Product.findByPk(req.params.id)
+       let categories = db.Category.findAll()
+       let productCategory = []
+        Promise.all([productOrder,categories]).then(function([productOrder,categories]){
+            categories.map(function (parametro, i, category) 
+            {if (category[i].id === productOrder.categoryId){
+                productCategory.push(category[i].category)
+           }})
+            return res.status(200).json({
+                data: {id: productOrder.id,
+                    name: productOrder.name,
+                    price: productOrder.price,
+                    discount: productOrder.discount,
+                    description: productOrder.description,
+                   imagen:"http://localhost:3333/img/products/"+ productOrder.image,
+
+                   detail:"http://localhost:3333/products/productDetail/"+ productOrder.id,
+
+                   //Cateogoria
+                   cateogory:productCategory,  
+                },
+                
+
+                
+                 
+
+
+            })
+            
+
+		})
     },
 
     createProduct: (req, res) => {
